bad3fd9b60ad859e3f05a1216d4aa7c33e102d3b
MarkStudio <markchen8024@gmail.com>
2020-05-13 15:50:34 +0800

    completing textfiled, usedWord list.

--- wordScramble/ContentView.swift
+++ wordScramble/ContentView.swift
@@ -9,8 +9,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var title = ""
+    @State private var usedNames = [String]()
+    @State private var newWord = ""
+    
     var body: some View {
-        Text("Hello, World!")
+       
+        NavigationView {
+            VStack {
+                TextField("Enter your new word.", text: $newWord, onCommit: addNewWord).textFieldStyle(RoundedBorderTextFieldStyle()).padding().autocapitalization(.none)
+                List(usedNames, id: \.self) {
+                    Text($0)
+                }
+            }
+        }
+        
+       
+    }
+    
+    func addNewWord() {
+        let ans = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines
+        )
+        newWord = ""
+        guard ans.count > 0 else {return}
+        usedNames.insert(ans, at: 0)
+        
     }
 }
 
